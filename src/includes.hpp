6d25ea4a7a431dab6237639d85a69d95e1da3d9a
Лозновенко Владислав Игоревич <loznovenko.vi@phystech.edu>
2021-10-31 02:21:13 +0300

    headers

---
+++ src/includes.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <filesystem>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <initializer_list>
